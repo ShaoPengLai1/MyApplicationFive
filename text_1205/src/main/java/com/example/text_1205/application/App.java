c7f570e0d66c368a26cd0cec8f2457a896706e26
ShaoPengLai <751337342@qq.com>
2018-12-07 15:51:18 +0800

    原生登录和第三方登录及分享，二维码展示

--- text_1205/src/main/java/com/example/text_1205/application/App.java
+++ text_1205/src/main/java/com/example/text_1205/application/App.java
@@ -8,6 +8,9 @@ import com.example.text_1205.utils.Global_capture;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.PlatformConfig;
+import com.umeng.socialize.UMShareAPI;
 
 public class App extends Application {
 
@@ -15,6 +18,8 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         //全局配置
+        UMConfigure.init(this,"5a12384aa40fa3551f0001d1","umeng",UMConfigure.DEVICE_TYPE_PHONE,"");
+        PlatformConfig.setQQZone("100424468", "c7394704798a158208a74ab60104f0ba");
         Global_capture.getInstance(getApplicationContext()).init(getApplicationContext());
         ImageLoader.getInstance().init(new ImageLoaderConfiguration.Builder(this)
 
--- text_1205/src/main/java/com/example/text_1205/ui/activity/MainActivity.java
+++ text_1205/src/main/java/com/example/text_1205/ui/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,7 @@ import android.text.TextUtils;
 import android.text.TextWatcher;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AbsListView;
@@ -29,6 +31,15 @@ import com.example.text_1205.bean.SignBean;
 import com.example.text_1205.persenter.IPresenterImpl;
 import com.example.text_1205.utils.NotNullUtls;
 import com.example.text_1205.view.IView;
+import com.umeng.commonsdk.UMConfigure;
+import com.umeng.socialize.ShareAction;
+import com.umeng.socialize.UMAuthListener;
+import com.umeng.socialize.UMShareAPI;
+import com.umeng.socialize.UMShareListener;
+import com.umeng.socialize.bean.SHARE_MEDIA;
+import com.umeng.socialize.media.UMImage;
+
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity implements IView,View.OnClickListener {
@@ -42,6 +53,7 @@ public class MainActivity extends AppCompatActivity implements IView,View.OnClic
     private String urlStr="http://www.xieast.com/api/user/login.php?username=%s&password=%s";
     private String name;
     private String pass;
+    private Button Asynchronous_login,QQ_Share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,13 +62,19 @@ public class MainActivity extends AppCompatActivity implements IView,View.OnClic
         initView();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        UMShareAPI.get(this).onActivityResult(requestCode, resultCode, data);
+    }
     private void initView() {
         phone_edit=findViewById(R.id.phone_edit);
         pass_edit=findViewById(R.id.pass_edit);
         remember=findViewById(R.id.remember);
         automatic=findViewById(R.id.automatic);
         submit=findViewById(R.id.submit);
-
+        QQ_Share=findViewById(R.id.QQ_Share);
+        Asynchronous_login=findViewById(R.id.Asynchronous_login);
         pass_edit.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -74,7 +92,8 @@ public class MainActivity extends AppCompatActivity implements IView,View.OnClic
             }
         });
         submit.setOnClickListener(this);
-
+        Asynchronous_login.setOnClickListener(this);
+        QQ_Share.setOnClickListener(this);
         /**
          * 密码显示和隐藏
          */
@@ -125,7 +144,37 @@ public class MainActivity extends AppCompatActivity implements IView,View.OnClic
                 }
             }
         });
+        //checkPermission();
     }
+
+    /**
+     * 动态添加权限，模拟器Android版本小于6.0的不用写
+     */
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String[] mPermissionList = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.CALL_PHONE,
+                    Manifest.permission.READ_LOGS,
+                    Manifest.permission.READ_PHONE_STATE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE,
+                    Manifest.permission.SET_DEBUG_APP,
+                    Manifest.permission.SYSTEM_ALERT_WINDOW,
+                    Manifest.permission.GET_ACCOUNTS,
+                    Manifest.permission.WRITE_APN_SETTINGS};
+            ActivityCompat.requestPermissions(this, mPermissionList, 123);
+        }
+    }
+    /**
+     * 动态添加权限回调，模拟器Android版本小于6.0的不用写，再有问的打死
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+    }
+
+
+
     @Override
     public void success(Object o) {
         //成功方法里强转为bean类
@@ -150,13 +199,8 @@ public class MainActivity extends AppCompatActivity implements IView,View.OnClic
     @Override
     public void onClick(View v) {
         int id = v.getId();
-
-
         switch (id){
-
             case R.id.submit:
-                //checkPermission();
-
                 /**
                  * 获取输入框的值
                  */
@@ -188,12 +232,82 @@ public class MainActivity extends AppCompatActivity implements IView,View.OnClic
                     editor.putBoolean("automatic_isCheck",true);
                     editor.commit();
                 }
+                break;
+            case R.id.QQ_Share:
+                UMImage image = new UMImage(MainActivity.this, R.drawable.umeng_socialize_qq);
+                new ShareAction(MainActivity.this)
+                        //分享的标题
+                        .withText("hello")
+                        //分享的图片
+                        .withMedia(image)
+                        //分享到哪，这里面只写了QQ，如有需要在后面添加
+                        .setDisplayList(SHARE_MEDIA.QQ)
+                        //设置回调
+                        .setCallback(shareListener)
+                        //打开分享面板
+                        .open();
+                break;
+            case R.id.Asynchronous_login:
+                //获得UMShareAPI实例
+                UMShareAPI umShareAPI=UMShareAPI.get(MainActivity.this);
+                //开始登录
+                //第一个参数：上下文
+                //第二个参数，登录哪种平台
+                //第三个参数，添加回调
+                umShareAPI.getPlatformInfo(MainActivity.this, SHARE_MEDIA.QQ, new UMAuthListener() {
+                    /**
+                     * 开始登录回调
+                     * @param share_media
+                     */
+                    @Override
+                    public void onStart(SHARE_MEDIA share_media) {
+                        Log.i("TAG","UMAuthListener onStart");
+                    }
 
+                    /**
+                     * 登录完成
+                     * @param share_media
+                     * @param i
+                     * @param map
+                     */
+                    @Override
+                    public void onComplete(SHARE_MEDIA share_media, int i, Map<String, String> map) {
+                        Log.i("TAG", "UMAuthListener onComplete");
+
+                        //获取名字
+                        String name  = map.get("screen_name");
+                        //获取头像
+                        String img  = map.get("profile_image_url");
+
+                        Log.i("TAG", "name is "+ name);
+                        Log.i("TAG", "img is "+ img);
+                    }
+
+                    /**
+                     * 登录失败
+                     * @param share_media
+                     * @param i
+                     * @param throwable
+                     */
+                    @Override
+                    public void onError(SHARE_MEDIA share_media, int i, Throwable throwable) {
+                        Log.i("TAG", "UMAuthListener onError" + throwable.getLocalizedMessage());
+                    }
+
+                    /**
+                     * 登录取消
+                     * @param share_media
+                     * @param i
+                     */
+                    @Override
+                    public void onCancel(SHARE_MEDIA share_media, int i) {
+                        Log.i("TAG", "UMAuthListener onCancel");
+                    }
+                });
                 break;
                     default:
                         break;
         }
-
     }
     /**
      * 解绑
@@ -203,6 +317,45 @@ public class MainActivity extends AppCompatActivity implements IView,View.OnClic
         super.onDestroy();
         mIPresenter.onDetach();
     }
+
+    private UMShareListener shareListener = new UMShareListener() {
+        /**
+         * @descrption 分享开始的回调
+         * @param platform 平台类型
+         */
+        @Override
+        public void onStart(SHARE_MEDIA platform) {
+            Log.i("TAG", "UMShareListener onStart");
+        }
+
+        /**
+         * @descrption 分享成功的回调
+         * @param platform 平台类型
+         */
+        @Override
+        public void onResult(SHARE_MEDIA platform) {
+            Log.i("TAG", "UMShareListener onResult");
+        }
+
+        /**
+         * @descrption 分享失败的回调
+         * @param platform 平台类型
+         * @param t 错误原因
+         */
+        @Override
+        public void onError(SHARE_MEDIA platform, Throwable t) {
+            Log.i("TAG", "UMShareListener onError");
+        }
+
+        /**
+         * @descrption 分享取消的回调
+         * @param platform 平台类型
+         */
+        @Override
+        public void onCancel(SHARE_MEDIA platform) {
+            Log.i("TAG", "UMShareListener onCancel");
+        }
+    };
 
 
 }
